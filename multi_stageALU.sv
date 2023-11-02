651338974ccdbb211c10fa2acbb8a24c601327d2
frahergeorge <147096550+frahergeorge@users.noreply.github.com>
2023-11-02 14:41:38 -0500

    Add files via upload

---
+++ multi_stageALU.sv
@@ -0,0 +1,22 @@
+module multi_stageALU(
+
+		//INPUTS//
+
+	input wire [9:0] OP,   //Operation Code, connects to Q0 from the register file 
+
+	input wire Ain,        //Ain, connects to Ain on the controller 
+
+	input wire Gin,        //Gin, connects to Gin on the controller
+
+	input wire Gout,       //Gout, connects to Gout on the controller
+
+	input wire [3:0] FN,   //Function, connects to ALUcont on the controller
+
+	   //OUTPUTS//
+
+	output wire [9:0] RES //connects to output logic and also is sent back to register
+
+);
+
+
+endmodule
