9ad2800271f6e812acdd75bf0008ccfd880f8cc1
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2023-11-02 14:42:23 -0500

    Add files via upload

--- Counter.sv
+++ Counter.sv
@@ -1,11 +1,25 @@
-module Counter #(parameter N = 8)
+module Counter
 (
 	input wire clk, // clock
-	input wire reset, // reset button
-	output wire [N-1:0] q // output of q
+	input wire DOWN, // 
+	input wire E, // enable switch
+	input wire CLRb, // Clear
+	input wire LD,	//	Load
+	input wire [N-1:0] D,	//
+	output wire [N-1:0] Q // output of Q
 );
 
-always_ff @ (posedge clk, posedge reset)
-	if (reset) q <= 0; // reset q to 0
-	else q <= q + 1; // inc q
+	parameter N = 8;
+
+always_ff @ (posedge clk, negedge CLRb) //toggle on postive edge of clock or negative edge of clear button
+	begin
+		if (~CLRb) Q <= 'b0;	// set Q back to 0
+		else if (E)	//if enable is active high
+		begin
+			if (LD) Q <= D;	// if LD store D into Q
+			else if (DOWN) Q <= Q - 1;	// if DOWN dec Q by 1
+			else Q <= Q + 1;	// else inc Q by 1
+		end
+		else Q <= Q;	//if enable is active low store Q back into Q
+	end
 endmodule
