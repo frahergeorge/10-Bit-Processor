fad1a6b4688b811d5ad7f8562292cec61515f8e3
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2023-11-06 15:20:44 -0600

    upcount2.sv

--- Counter.sv
+++ Counter.sv
@@ -1,25 +1,13 @@
-module Counter
+module upcount2
 (
-	input wire clk, // clock
-	input wire DOWN, // 
-	input wire E, // enable switch
-	input wire CLRb, // Clear
-	input wire LD,	//	Load
-	input wire [N-1:0] D,	//
-	output wire [N-1:0] Q // output of Q a.k.a CNT
+	input wire clkb, // clock button press
+	input wire clrb, // reset button press
+	output wire [1:0] count // counter 
 );
 
-	parameter N = 8;
-
-always_ff @ (posedge clk, negedge CLRb) //toggle on postive edge of clock or negative edge of clear button
+always_ff @ (posedge clkb, posedge clrb) //toggle on postive edge of clock or negative edge of clear button
 	begin
-		if (~CLRb) Q <= 'b0;	// set Q back to 0
-		else if (E)	//if enable is active high
-		begin
-			if (LD) Q <= D;	// if LD store D into Q
-			else if (DOWN) Q <= Q - 1;	// if DOWN dec Q by 1
-			else Q <= Q + 1;	// else inc Q by 1
-		end
-		else Q <= Q;	//if enable is active low store Q back into Q
+		if (clrb) count <= 0;	// sets count to zero if reset is high
+		else count <= count + 1;	// inc counts
 	end
 endmodule
