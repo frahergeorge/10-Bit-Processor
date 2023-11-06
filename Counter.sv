ee92ddd7c095c0873a089296660aefb79defdcae
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2023-11-06 16:32:25 -0600

    Counter.sv

--- Counter.sv
+++ Counter.sv
@@ -1,13 +1,23 @@
 module upcount2
 (
 	input wire clkb, // clock button press
+	input wire clk,  // 50MHZ clock
 	input wire clrb, // reset button press
-	output wire [1:0] count // counter 
+	output wire [1:0]count, // counter 
+	output logic [6:0] counter
 );
 
-always_ff @ (posedge clkb, posedge clrb) //toggle on postive edge of clock or negative edge of clear button
+wire DB, DBR;
+
+debouncer (.A_noisy(clkb), .CLK50M(clk), .A(DB));
+debouncer (.A_noisy(clrb), .CLK50M(clk), .A(DBR));
+
+always_ff @ (posedge DB) //toggle on postive edge of clock or postive edge of clear button
 	begin
-		if (clrb) count <= 0;	// sets count to zero if reset is high
+		if (DBR) count <= 0;	// sets count to zero if reset is high
 		else count <= count + 1;	// inc counts
 	end
+	
+decimal7decoder (.SW(count), .numHEX(counter));
+	
 endmodule
