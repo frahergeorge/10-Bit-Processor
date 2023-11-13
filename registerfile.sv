55dfb1363a5ab1fe752379278f7a672381a168f2
frahergeorge <147096550+frahergeorge@users.noreply.github.com>
2023-11-13 16:47:18 -0600

    Update registerfile.sv

--- registerfile.sv
+++ registerfile.sv
@@ -1,30 +1,26 @@
-module registerfile(
-
-		//INPUTS//
-		
-	input wire [9:0] D,     //10 bit input signal
-	
-	input wire [1:0] WRA,   //Write Address
-	
-	input wire ENW,         //Enable Write
-	
-	input wire [1:0] RDA0,  //Read Address 0 
-	
-	input wire ENR0,        //Enable read signal to read from the register file 
-	
-	input wire [1:0] RDA1,  //Read Address 1
-	
-	input wire ENR1,		//Enable read signal to read from the register file 
-	
-	input wire Clkb,        //Debounced Clock 
-
-		//OUTPUTS//
-   
-	output wire [9:0] Q0,  //Q not, 10 bit output signal
-
-	output wire [9:0] Q1   //Q, 10 bit output signal
+module registerfile
+(
+  input wire [9:0] D,
+  input wire [1:0] WRA,
+  input wire ENW,
+  input wire [1:0] RDA0,
+  input wire ENR0,
+  input wire [1:0] RDA1,
+  input wire ENR1,
+  input wire Clkb,
+  output wire [9:0] Q0,
+  output wire [9:0] Q1
 );
 
-
+  reg [9:0] regfile [0:3]; // 4 registers, each 10 bits wide
+  
+  always @(posedge Clkb) begin
+    if (ENW) begin
+      regfile[WRA] <= D;
+    end
+  end
+  
+  assign Q0 = (ENR0) ? regfile[RDA0] : 10'bZ;
+  assign Q1 = (ENR1) ? regfile[RDA1] : 10'bZ;
 
 endmodule
