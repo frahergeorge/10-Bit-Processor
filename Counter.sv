f6d24c51f63303d927241b68eebc4f785447834b
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2023-11-14 13:25:38 -0600

    upcount2

--- Counter.sv
+++ Counter.sv
@@ -1,21 +1,28 @@
 module upcount2
 (
-	input wire clkb, // clock button press
-	input wire clk,  // 50MHZ clock
-	output wire [1:0]count, // counter 
-	output logic [6:0] counter
+  input wire clkb, // clock button press
+  input wire clk,  // 50MHz clock
+  output wire [1:0] count = 2'b00, // counter
+  output logic [6:0] counter
 );
 
-wire DB;
+  wire DB;
+ 
+  debouncer deb (.A_noisy(clkb), .CLK50M(clk), .A(DB));
+ 
+  always_ff @(negedge DB) begin
+    begin
+        // Increment condition
+        case (count)
+            2'b00: count = 2'b01;
+            2'b01: count = 2'b10;
+            2'b10: count = 2'b11;
+            2'b11: count = 2'b00;
+            default: count = 2'b00; // Default case to handle unexpected values
+        endcase
+    end
+  end
+ 
+  decimal7decoder dec (.SW(count), .numHEX(counter));
 
-debouncer (.A_noisy(clkb), .CLK50M(clk), .A(DB));
-
-always_ff @ (posedge DB) //toggle on postive edge of clock or postive edge of clear button
-	begin
-		if (~DB) count <= 0;	// sets count to zero if reset is high
-		else count <= count + 1;	// inc counts
-	end
-	
-decimal7decoder (.SW(count), .numHEX(counter));
-	
 endmodule
